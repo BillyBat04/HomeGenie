073c7b7b5ab0de6d7770aeeed83affcf4e79a927
BillyBat04 <billybat04@localhost.localdomain>
2026-03-29 13:50:53 +0700

    Fix CI/CD

--- gateway-service/src/main/java/com/homegenie/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/com/homegenie/gateway/GatewayApplication.java
@@ -3,7 +3,6 @@ package com.homegenie.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class GatewayApplication {
     
